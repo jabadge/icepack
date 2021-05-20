820a206252bd4c67e2421b9ec789aee053760417
Jessica Badgeley <badgeley@uw.edu>
2021-05-20 15:59:33 -0700

    initial age model test commit

--- icepack/models/age_transport.py
+++ icepack/models/age_transport.py
@@ -18,30 +18,42 @@ from firedrake import (
     inner, grad, div, dx, ds, ds_t, ds_v, dS_v, dS_h, dS, sqrt, det, min_value, max_value, conditional
 )
 from icepack.constants import year
-from icepack.utilities import eigenvalues, vertical_velocity, get_kwargs_alt
+from icepack.utilities import eigenvalues, vertical_velocity, get_kwargs_alt, add_kwarg_wrapper
+
+def velocity_3D(**kwargs):
+    keys = ('thickness', 'velocity')
+    keys_alt = ('h','u')
+    h, u = get_kwargs_alt(kwargs, keys, keys_alt)
+
+    Q = h.function_space()
+    V = u.function_space()
+    mesh = Q.mesh()
+    xdegree_u, zdegree_u = u.ufl_element().degree()
+    W = firedrake.FunctionSpace(mesh, family='CG', degree=2, vfamily='GL', vdegree=1+V.ufl_element().degree()[1])
+    w = firedrake.interpolate(vertical_velocity(u,h),W)
+    V3D = firedrake.VectorFunctionSpace(mesh, dim=3, family='CG', degree=xdegree_u, vfamily='GL',vdegree=zdegree_u+1)
+    u3D = firedrake.Function(V3D).interpolate(firedrake.as_vector((u[0],u[1],w)))
+    return u3D 
 
 
 class AgeTransport:
     def __init__(
         self,
-        max_age=2000000.0
+        max_age=2000000.0,
+        velocity_3D=velocity_3D
     ):
         self.max_age = max_age
+        self.velocity_3D = add_kwarg_wrapper(velocity_3D)
 
-
-        
     def flux(self, **kwargs):
         keys = ('thickness', 'velocity', 'age')
         keys_alt = ('h','u','age')
         h, u, q = get_kwargs_alt(kwargs, keys, keys_alt)
 
         Q = h.function_space()
-        V = u.function_space()
         mesh = Q.mesh()
         U = q.function_space()
         φ = firedrake.TestFunction(U)
-
-        w = firedrake.interpolate(vertical_velocity(u,h),U)
 
         xdegree_u, zdegree_u = u.ufl_element().degree()
         degree_h = h.ufl_element().degree()[0]
@@ -49,8 +61,8 @@ class AgeTransport:
         metadata = {'quadrature_degree': degree}
         ice_front_ids = tuple(kwargs.pop('ice_front_ids', ()))
         ds_terminus = ds_v(domain=mesh, subdomain_id=ice_front_ids, metadata=metadata)
-        V3D = firedrake.VectorFunctionSpace(mesh,dim=3, family='CG',degree=xdegree_u,vfamily='GL',vdegree=zdegree_u+1)
-        u3D = firedrake.Function(V3D).interpolate(firedrake.as_vector((u[0],u[1],w)))
+
+        u3D = velocity_3D(**kwargs)
 
         n = firedrake.FacetNormal(mesh)
         u_n = max_value(0, inner(u3D, n))
